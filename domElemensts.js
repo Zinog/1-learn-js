d59286eacf51574c88b12ba4b4450a019894c13f
Zinok <chevohnoe@mail.ru>
2023-06-24 01:41:11 +0300

    работа с dom-elements

--- domElemensts.js
+++ domElemensts.js
@@ -17,3 +17,42 @@ let makeElement = function (tagName, className, text) {
     return element;  
   };
   
+  let button = document.querySelector('.button');
+  //Функция addEventListener() говорит о том, что мы добавляем обработчик события на элемент.
+  button.addEventListener('click', function (evt) {
+    // Инструкции
+    // Отменяем действие по умолчанию
+  evt.preventDefault();
+  evt.keyCode
+  });
+//Объект event — параметр функции-обработчика event = evt, содержит метод, который отменяет действие по умолчанию: preventDefault()
+//                                                                         код клавиши которую нажал пользователь содержит keyCode
+//'click' клик мышью
+//'keydown' клик клавиатурой
+
+//document.querySelector('.класс');             чтобы найти на странице элемент, нужно использовать этот метод
+//document.querySelectorAll('селектор');        поиск всех элементов, подходящих по селектору, его можно перебирать с помощью цикла for.
+//document.createElement('селектор');           создаём новый элемент
+let card = document.createElement('li');
+card.classList.add('card');
+list.appendChild(card);
+//элемент.classList.remove('класс');            убирает с элемента тот класс, который указан в скобках
+//элемент.classList.add('класс');               добавить элементу класс
+//элемент.classList.toggle('класс');            убирает у элемента указанный класс, если он есть, и добавляет, если этого класса нет
+//класс.appendChild(элемент);                   добавление нового элемента, вставляет его в конец родительского элемента
+//button.textContent = 'Здесь был Кекс. Мяу!';  textContent хранит в себе текстовое содержимое элемента. Свойствам можно 
+//                                              присваивать новые значения
+//onclick                                       Инструкции, которые должны будут выполниться, когда событие произойдёт, 
+//                                              располагают между фигурных скобок
+button = document.querySelector('button');
+button.onclick = function() {
+  console.log('Кнопка нажата!');
+};
+//onsubmit                                      отвечает за обработку отправки формы  
+let form = document.querySelector('form');
+form.onsubmit = function() {
+  console.log('Форма отправлена!');
+};
+
+
+
--- gllacy/gllacy.js
+++ gllacy/gllacy.js
@@ -1,4 +1,4 @@
-/* Мяу! На сайте магазина мороженого надо отображать актуальное состояние товаров: «в наличии», «нет в наличии», «хит».
+/*На сайте магазина мороженого надо отображать актуальное состояние товаров: «в наличии», «нет в наличии», «хит».
 Данные по продуктам хранятся в массиве с объектами assortmentData, каждый объект соответствует одному товару и содержит свойства:
 - inStock. Если значение true — мороженое в наличии, если false — товара в наличии нет.
 - isHit. Если значение true — мороженое самое популярное среди покупателей.
@@ -53,3 +53,108 @@ var assortmentData = [
   };
   
   updateCards(assortmentData);
+
+/* Помнишь магазин мороженого? Нужно создать карточки товаров, основываясь на данных, полученных с сервера.
+Данные — массив объектов cardsData, один элемент соответствует одному товару. У каждого объекта есть следующие свойства:
+- inStock — доступность товара. Если значение true — товар доступен (для такого продукта верстальщик подготовил класс good--available), если false — продукта нет в наличии (товар с классом good--unavailable).
+- imgUrl — ссылка на изображение товара.
+- text — название продукта.
+- price — цена.
+- isHit — является ли товар хитом продаж. Если значение true — продукт «хитовый». Для такого товара подготовлен класс good--hit.
+- specialOffer — специальное предложение, которое есть только у хита продаж. Должно находиться в абзаце с классом good__special-offer и быть самым последним дочерним элементом в карточке.
+Вот пример вёрстки одной карточки в каталоге:
+<ul class="goods">
+  <li class="good">
+    <h2 class="good__description">Сливочно-кофейное с кусочками шоколада</h2>
+    <img class="good__image" src="gllacy/choco.jpg" alt="Сливочно-кофейное с кусочками шоколада">
+    <p class="good__price">110₽/кг</p>
+  </li>
+  ...
+</ul>
+Обрати внимание, что текст в атрибуте alt у изображения должен быть таким же, как и название товара.
+Создай функцию renderCards, которая будет принимать на вход массив данных, вызови её, передав cardsData, и отрисуй на странице карточки мороженого.
+*/
+var cardsData = [
+    {
+      inStock: true,
+      imgUrl: 'gllacy/choco.jpg',
+      text: 'Сливочно-кофейное с кусочками шоколада',
+      price: 310,
+      isHit: true,
+      specialOffer: 'Двойная порция сиропа бесплатно!'
+    },
+    {
+      inStock: false,
+      imgUrl: 'gllacy/lemon.jpg',
+      text: 'Сливочно-лимонное с карамельной присыпкой',
+      price: 125,
+      isHit: false
+    },
+    {
+      inStock: true,
+      imgUrl: 'gllacy/cowberry.jpg',
+      text: 'Сливочное с брусничным джемом',
+      price: 170,
+      isHit: false
+    },
+    {
+      inStock: true,
+      imgUrl: 'gllacy/cookie.jpg',
+      text: 'Сливочное с кусочками печенья',
+      price: 250,
+      isHit: false
+    },
+    {
+      inStock: true,
+      imgUrl: 'gllacy/creme-brulee.jpg',
+      text: 'Сливочное крем-брюле',
+      price: 190,
+      isHit: false
+    }
+  ];
+  
+    var makeElement = function (tagName, className, text) {
+      var element = document.createElement(tagName);
+      element.classList.add(className);
+      if (text) {
+        element.textContent = text;
+      }
+      return element;
+    };
+    
+    var createCard = function (product) {
+      var listItem = makeElement('li', 'good');
+    
+      var title = makeElement('h2', 'good__description', product.text);
+      listItem.appendChild(title);
+    
+      var picture = makeElement('img', 'good__image');
+      picture.src = product.imgUrl;
+      picture.alt = product.text;
+      listItem.appendChild(picture);
+    
+      var price = makeElement('p', 'good__price', product.price + '₽/кг');
+      listItem.appendChild(price);
+    
+      var availabilityClass = 'good--available';
+      if (!product.inStock) {
+        availabilityClass = 'good--unavailable';
+      }
+      listItem.classList.add(availabilityClass);
+    
+      if (product.isHit) {
+        listItem.classList.add('good--hit');
+        var specialPrice = makeElement('p', 'good__special-offer', product.specialOffer);
+        listItem.appendChild(specialPrice);
+      }
+    
+      return listItem;
+    };
+    
+    var cardList = document.querySelector('.goods');
+    
+    for (var i = 0; i < cardsData.length; i++) {
+      var cardItem = createCard(cardsData[i]);
+      cardList.appendChild(cardItem);    
+      }
+      
